a3a71e71c2a1dc953f0fee0b3faaaa25fcc4acf1
JOLenizo <82566107+Jackilynlenizo@users.noreply.github.com>
2023-03-06 15:38:17 +0800

    made search bar expandable

--- src/components/HomeContent.js
+++ src/components/HomeContent.js
@@ -6,6 +6,7 @@ import { Throwback } from "./Throwback";
 import ThrowbackData from "../data/ThrowbackData";
 import slugify from "react-slugify";
 import { motion } from "framer-motion";
+import {BiSearchAlt2} from 'react-icons/bi'
 
 export const HomeContent = () => {
   const [trSearch, setTrSearch] = useState("")
@@ -41,14 +42,18 @@ export const HomeContent = () => {
 
   return (
     <div className="relative top-0 bgcolor pb-5">
-      <div className="md:flex justify-between items-center px-[90px] pb-2">
-        <motion.h1 whileHover={{ scale: 1.1 }} transition={{ type: "spring", stiffness: 400, damping: 15 }} className="font-outfit font-bold text-[34px] text-white flex py-4 gap-2">
+      <div className="md:flex md:pb-0  justify-between items-center px-[90px]">
+        <motion.h1 whileHover={{ scale: 1.1 }} transition={{ type: "spring", stiffness: 400, damping: 15 }} className="font-outfit font-bold text-[34px] text-white flex md:py-4 pt-4 gap-2">
           Trending <span className=" text-[#FBC94A]">this week </span>
         </motion.h1>
-        <input type="search" placeholder="Search Anime..." className="rounded-md pl-4" onChange={(e) => setTrSearch(e.target.value) }/>
+        <form className="md:pb-0 pb-3">
+          <BiSearchAlt2 color="#106580" className="absolute z-10 md:inset-y-[29.5px] inset-y-[70.3px] py-1 w-5 h-6 border-r border-[#106580] peer-focus:[#FBC94A]" />
+            <input type="search" placeholder="Search Anime..." 
+              className="relative peer outline-none rounded-full pl-12 cursor-pointer w-12 h-8 focus:w-52 focus:pl-7 focus:pr-4 " onChange={(e) => setTrSearch(e.target.value) }/>
+        </form>
       </div>
 
-        <div className="flex flex-nowrap items-center space-x-5 overflow-x-auto scrollbar-hide px-[90px] py-4 scroll-pl-[90px] snap-x">
+        <div className="flex flex-nowrap items-center space-x-5 overflow-x-auto scrollbar-hide px-[90px] pb-4 scroll-pl-[90px] snap-x">
            {trendingdata}
         </div>
 
@@ -57,7 +62,11 @@ export const HomeContent = () => {
             <motion.h1 whileHover={{ scale: 1.1 }} transition={{ type: "spring", stiffness: 400, damping: 15 }} className="font-outfit font-bold text-[34px] py-3 text-white">
               Throwback Anime!
             </motion.h1>
-            <input type="search" placeholder="Search Anime..." className="rounded-md pl-4" onChange={(e) => setTbSearch(e.target.value) }/>
+            <form className="md:pb-0 pb-3">
+              <BiSearchAlt2 color="#106580" className="absolute z-10 md:inset-y-[26px] inset-y-[78.3px] py-1 w-5 h-6 border-r border-[#106580] peer-focus:[#FBC94A]" />
+              <input type="search" placeholder="Search Anime..." 
+                className="relative peer outline-none rounded-full pl-12 cursor-pointer w-12 h-8 focus:w-52 focus:pl-7 focus:pr-4 " onChange={(e) => setTbSearch(e.target.value) }/>
+            </form>
           </div>
 
           <div className="flex flex-nowrap z-0 items-center space-x-5 overflow-x-auto scrollbar-hide px-[90px] scroll-pl-[90px] snap-x">
